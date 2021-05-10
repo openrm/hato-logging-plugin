0aeec201f8c4b6aa8ceaae690f8e616c35ee0b0c
robjweiss <rweiss55@gmail.com>
2021-05-10 17:02:19 +0900

    plugin to log messages

---
+++ src/log.spec.js
@@ -0,0 +1,137 @@
+const assert = require('assert');
+const EventEmitter = require('events');
+
+const { Client, plugins } = require('hato');
+const Log = require('./log');
+
+// Externally resolvable promise
+class Deferred {
+    constructor() {
+        this.promise = new Promise((resolve, reject) => {
+            this.reject = reject;
+            this.resolve = resolve;
+        });
+
+        this.then = this.promise.then.bind(this.promise);
+        this.catch = this.promise.catch.bind(this.promise);
+    }
+}
+
+describe('log plugin', () => {
+    let client;
+    let emitter;
+
+    beforeEach(async() => {
+        emitter = new EventEmitter();
+
+        const logger = (data) => {
+            emitter.emit('log', data);
+        };
+
+        client = await new Client('amqp://guest:guest@127.0.0.1:5672', {
+            plugins: [new plugins.Encoding('json'), new Log(logger)]
+        })
+            .start();
+    });
+
+    afterEach(() => client.close());
+
+    describe('publishes', () => {
+        it('it should log messages published to a direct exchange', (done) => {
+            const deferred = new Deferred();
+
+            // Ensure logged data is as expected
+            const check = (data) => {
+                assert.deepStrictEqual(data.content, { string: 'string' });
+                assert.strictEqual(data.routingKey, 'a.routing.key');
+                assert.strictEqual(data.exchange, 'amq.direct');
+                assert.strictEqual(data.action, 'publish');
+            };
+
+            // Message is logged twice, once when published, once when consumed and acknowledged
+            emitter.once('log', (data) => deferred.then(() => {
+                try {
+                    check(data);
+                    done();
+                } catch (error) {
+                    done(error);
+                }
+            }));
+
+            // Publish message
+            client
+                .type('direct')
+                .publish('a.routing.key', { string: 'string' })
+                .then(deferred.resolve)
+                .catch(done);
+        });
+
+        it('it should log messages published to a topic exchange', (done) => {
+            const deferred = new Deferred();
+
+            // Ensure logged data is as expected
+            const check = (data) => {
+                assert.deepStrictEqual(data.content, { string: 'string' });
+                assert.strictEqual(data.routingKey, 'a.routing.key');
+                assert.strictEqual(data.exchange, 'amq.topic');
+                assert.strictEqual(data.action, 'publish');
+            };
+
+            // Message is logged twice, once when published, once when consumed and acknowledged
+            emitter.once('log', (data) => deferred.then(() => {
+                try {
+                    check(data);
+                    done();
+                } catch (error) {
+                    done(error);
+                }
+            }));
+
+            // Subscribe and acknowledge message
+            client
+                .queue('foo', { durable: false, exclusive: true })
+                .subscribe('a.routing.key', (msg) => msg.ack())
+                .on('error', done);
+
+            // Publish message
+            client
+                .type('topic')
+                .publish('a.routing.key', { string: 'string' })
+                .then(deferred.resolve)
+                .catch(done);
+        });
+    });
+
+    describe('consumption', () => {
+        it('it should log messages consumed', (done) => {
+            // Ensure logged data is as expected
+            const check = (data) => {
+                assert.deepStrictEqual(data.content, { string: 'string' });
+                assert.strictEqual(data.routingKey, 'a.routing.key');
+                assert.strictEqual(data.exchange, 'amq.topic');
+                assert.strictEqual(data.action, 'consume');
+                done();
+            };
+
+            // Check the message when it is logged
+            emitter.on('log', (data) => {
+                if (data.action === 'consume') check(data);
+            });
+
+            // Subscribe and acknowledge message
+            client
+                .type('topic')
+                .queue('foo', { durable: false, exclusive: true })
+                .subscribe('a.routing.key', (msg) => {
+                    msg.ack();
+                })
+                .on('error', done);
+
+            // Publish message
+            client
+                .type('topic')
+                .publish('a.routing.key', { string: 'string' })
+                .catch(done);
+        });
+    });
+});
