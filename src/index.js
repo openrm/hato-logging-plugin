d6d6befa675c3d00c3c7c4b8c97718cdb6dc1057
Gensuke Toshiro <toshiro@openrm.co.jp>
2022-04-02 19:00:40 +0900

    trigger logging in publish hook or in callback

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const shimmer = require('shimmer');
+const Args = require('amqplib/lib/api_args');
 const { plugins, constants } = require('hato');
-const { Scopes: { CONNECTION, SUBSCRIPTION } } = constants;
+const { Scopes: { CONNECTION, SUBSCRIPTION, PUBLICATION } } = constants;
 
 module.exports = class extends plugins.Base {
 
@@ -21,12 +21,9 @@ module.exports = class extends plugins.Base {
             ((level, struct, msg) => this.logger[level](struct, msg));
 
         this.scopes[SUBSCRIPTION] = this.onConsume();
+        this.scopes[PUBLICATION] = this.onPublish();
         this.scopes[CONNECTION] = this.onConnect();
         this.hooks[CONNECTION] = this.onOpen();
-    }
-
-    destroy() {
-        if (this.unpatch) this.unpatch();
     }
 
     onConnect() {
@@ -40,7 +37,6 @@ module.exports = class extends plugins.Base {
         return (conn) => {
             const { connection } = conn,
                 { stream: socket, serverProperties } = connection;
-            this.unpatch = this.patchConnection(connection);
             this.log('debug', { serverProperties }, 'Connected to broker.');
             Object.assign(this.connFields, {
                 protocolVersion: '0.9.1',
@@ -80,6 +76,41 @@ module.exports = class extends plugins.Base {
         };
     }
 
+    logPublish(exchange, routingKey, content, options, err) {
+        // Log with the provided function
+        try {
+            const properties = Args.publish(exchange, routingKey, options);
+            this.log('info', {
+                ...this.connFields,
+                err,
+                command: 'publish',
+                exchange,
+                routingKey,
+                properties: {
+                    ...properties,
+                    headers: Object.getPrototypeOf(properties.headers)
+                },
+                content: this.serializeContent(content)
+            }, 'Message published.');
+        } catch (err) {
+            this.log('warn', { err }, '[AMQP:log] Message logging failed.');
+        }
+    }
+
+    onPublish() {
+        const plugin = this;
+        return (publish) => (exchange, routingKey, content, options, cb) => {
+            if (typeof cb !== 'function') {
+                plugin.logPublish(exchange, routingKey, content, options);
+                return publish(exchange, routingKey, content, options);
+            }
+            return publish(exchange, routingKey, content, options, function(err) {
+                cb(err);
+                plugin.logPublish(exchange, routingKey, content, options, err);
+            });
+        };
+    }
+
     extractProtocol(url) {
         if (typeof url === 'object') {
             this.connFields.protocol = url.protocol;
@@ -106,41 +137,6 @@ module.exports = class extends plugins.Base {
         } catch {
             return undefined;
         }
-    }
-
-    patchSendMessage(original) {
-        const plugin = this;
-        return function loggedSendMessage() {
-            // Log with the provided function
-            try {
-                const [, , fields, , properties, content] = arguments;
-                plugin.log('info', {
-                    ...this.connFields,
-                    command: 'publish',
-                    exchange: fields.exchange,
-                    routingKey: fields.routingKey,
-                    properties: {
-                        ...properties,
-                        headers: Object.getPrototypeOf(properties.headers)
-                    },
-                    content: plugin.serializeContent(content)
-                }, 'Message published.');
-            } catch (err) {
-                plugin.log('warn', { err }, '[AMQP:log] Message logging failed.');
-            }
-            return original.apply(this, arguments);
-        };
-    }
-
-    patchConnection(connection) {
-        shimmer.wrap(
-            connection.constructor.prototype,
-            'sendMessage',
-            this.patchSendMessage.bind(this));
-        return () => shimmer.unwrap(
-            connection.constructor.prototype,
-            'sendMessage',
-            this.patchSendMessage.bind(this));
     }
 
 };
